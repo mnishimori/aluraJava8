d1190e3835c51c2b8f051f2eecab0335a8695e79
mnishimori <mnishimori@yahoo.com.br>
2022-08-22 08:38:40 -0300

    Streams

---
+++ src/java8/ExemploCursos.java
@@ -0,0 +1,43 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ExemploCursos {
+
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+		System.out.println(cursos);
+	}
+
+}
+
+class Curso {
+    private String nome;
+    private int alunos;
+
+	public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+    
+    @Override
+	public String toString() {
+		return "Curso [nome=" + nome + ", alunos=" + alunos + "]";
+	}
+}
